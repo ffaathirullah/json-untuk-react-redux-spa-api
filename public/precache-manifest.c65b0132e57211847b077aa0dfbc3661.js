e212d88c06ff768e9b79aa76ef7a2357c05daa9b
Fachrul <ffaathirullah1@gmail.com>
2020-08-11 10:58:09 +0700

    first commit

---
+++ public/precache-manifest.c65b0132e57211847b077aa0dfbc3661.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca224f1395e0aeef31722518e5400be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9518d20edde9de9fe83b",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "197d3ac7000be01de593",
+    "url": "/static/css/main.d90ed22d.chunk.css"
+  },
+  {
+    "revision": "9518d20edde9de9fe83b",
+    "url": "/static/js/2.73b28b14.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.73b28b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197d3ac7000be01de593",
+    "url": "/static/js/main.764dbf3e.chunk.js"
+  },
+  {
+    "revision": "80524adf7a680459ccea",
+    "url": "/static/js/runtime-main.f264c7c6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
